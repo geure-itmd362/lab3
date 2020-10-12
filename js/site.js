206b7499b1e9f220aeed3152815f3fa95f97d59c
Greg Eure <geure@hawk.iit.edu>
2020-10-12 17:39:41 -0500

    Added greyscale color scheme and animated submit

--- js/site.js
+++ js/site.js
@@ -1,23 +1,37 @@
 // jQuery 3.x-style ready event and locally scoped $
-jQuery(function($) {
-  $('html').removeClass('nojs');
-  $('html').addClass('hasjs');
-  $('#email').on('focus', function() {
-    $('#input-email label').addClass('active');
-  });
-  $('#email').on('blur', function() {
-    if($('#email').val().length == 0) {
-      $('#input-email label').removeClass('active');
-    }
-  });
-  $("#alt-box").on("click", function () {
-    if (!$("#age-verification").attr("checked")) {
-      $("#age-verification").prop("checked", true);
-      $("#alt-box").html("<p>&#10004;</p>");
-    }
-    if ($("#age-verification").attr("checked")) {
-      $("#age-verification").prop("checked", false);
-      $("#alt-box").html("<p>&#10004;</p>");
-    }
+
+$(document).ready(function () {
+  jQuery(function($) {
+    $('html').removeClass('nojs');
+    $('html').addClass('hasjs');
+    $('#email').on('focus', function() {
+      $('#input-email label').addClass('active');
+    });
+    $('#email').on('blur', function() {
+      if($('#email').val().length == 0) {
+        $('#input-email label').removeClass('active');
+      }
+    });
+
+      //$("#alt-box").on("click", function () {
+      //  if ($("#age-verification").prop("checked", true)) {
+      //    $("#age-verification").prop("checked", false);
+       //   $("#alt-box").html("<p>&#10004;</p>");
+      //  } else {
+      //    $("#age-verification").prop("checked", true);
+       //   $("#alt-box").html("<p>&#32;</p>");
+       // }
+      //});
+
+
+        $('#alt-box').click(function (event) {
+        if (!(this.checked)) {
+          $('#age-verification').prop('checked', true); //check 
+          $("#alt-box").html("<p>&#10004;</p>");
+        } else {
+          $('#age-verification').prop('checked', false); //uncheck              
+        }
+    });
+
   });
 });
